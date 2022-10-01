daaf043bd2ee7cf3facd5e4955c7aceb9a9b479e
Saobby <101960076+Saobby@users.noreply.github.com>
2022-10-01 13:34:44 +0800

    Update upload_avatar.js

--- static/js/upload_avatar.js
+++ static/js/upload_avatar.js
@@ -1,28 +1,6 @@
 function upload_avatar(){
-    document.getElementById("upload-btn").innerHTML = "上传中";
-    document.getElementById("upload-btn").disabled = true;
-    var avatar_file = document.getElementById("avatar_img_file").files[0];
-    var fd = new FormData();
-    var xhr = new XMLHttpRequest();
-    xhr.open('post','https://saobbygguserdata.pythonanywhere.com/api/upload_image',true);
-    xhr.onreadystatechange=function (){
-        if(this.readyState==4){
-            ret_json = JSON.parse(this.responseText);
-            if (ret_json.success){
-                document.getElementById("avatar_url").value = ret_json.image_url;
-                document.getElementById("avatar_img").src = ret_json.image_url;
-            }
-            document.getElementById("upload_result").innerHTML = ret_json.message;
-            document.getElementById("upload-btn").innerHTML = "上传";
-            document.getElementById("upload-btn").disabled = false;
-        }
-    }
-    xhr.upload.onprogress=function (ev){
-        if(ev.lengthComputable){
-            var precent=100 * ev.loaded/ev.total;
-        document.getElementById("upload_result").innerHTML=Math.floor(precent)+"%";
-        }
-    }
+get_captcha_img();
+document.getElementById("captcha-window").hidden=false;
 }
 
 function set_avatar(){
@@ -31,11 +9,6 @@ if (avatar_url == ""){
 document.getElementById("set_result").innerHTML="头像链接不能为空!";
 return;
 }
-get_captcha_img();
-document.getElementById("captcha-window").hidden=false;
-};
-
-function complete_captcha(){
 document.getElementById("save-btn").disabled=true;
 document.getElementById("save-btn").innerHTML="请稍候";
 var avatar_url = document.getElementById("avatar_url").value;
@@ -57,4 +30,35 @@ xhr.onreadystatechange = function() {
         }
     }
 }
+};
+
+function complete_captcha(){
+    document.getElementById("upload-btn").innerHTML = "上传中";
+    document.getElementById("upload-btn").disabled = true;
+    var avatar_file = document.getElementById("avatar_img_file").files[0];
+    var captcha_token = document.getElementById("captcha-token").value;
+    var fd = new FormData();
+    var xhr = new XMLHttpRequest();
+    xhr.open('post','https://saobbygguserdata.pythonanywhere.com/api/upload_image',true);
+    xhr.onreadystatechange=function (){
+        if(this.readyState==4){
+            ret_json = JSON.parse(this.responseText);
+            if (ret_json.success){
+                document.getElementById("avatar_url").value = ret_json.image_url;
+                document.getElementById("avatar_img").src = ret_json.image_url;
+            }
+            document.getElementById("upload_result").innerHTML = ret_json.message;
+            document.getElementById("upload-btn").innerHTML = "上传";
+            document.getElementById("upload-btn").disabled = false;
+        }
+    }
+    xhr.upload.onprogress=function (ev){
+        if(ev.lengthComputable){
+            var precent=100 * ev.loaded/ev.total;
+        document.getElementById("upload_result").innerHTML=Math.floor(precent)+"%";
+        }
+    }
+    fd.append("image", avatar_file);
+    fd.append("captcha_token", captcha_token);
+    xhr.send(fd);
 };
