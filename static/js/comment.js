9eaab3af9ca69ebeed24760a144b466481de6766
Saobby <101960076+Saobby@users.noreply.github.com>
2023-04-20 21:59:45 +0800

    Update comment.js

--- static/js/comment.js
+++ static/js/comment.js
@@ -145,9 +145,7 @@ var comment_page_index = 0,
 if (2 === nurl.length) {
 	var args = nurl[1].split("&");
 	place_id = args[0], is_in_array(args, "comments-only") && (
-		for (var p = document.getElementsByClassName("hco"), q = 0; q < p.length; q++) {
-			//p[q].hidden = !0
-		});
+		true);
 	for (var scroll_to = void 0, i = 0; i < args.length; i++) {
 		var t = args[i].split("=");
 		2 === t.length && "scroll_to" === t[0] && (scroll_to = t[1])
