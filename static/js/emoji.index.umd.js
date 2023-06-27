96d6a248eff5a90183a5c3c5cb31e4a10e72a055
Saobby <101960076+Saobby@users.noreply.github.com>
2023-06-27 17:41:52 +0800

    Add files via upload

---
+++ static/js/emoji.index.umd.js
@@ -0,0 +1,60 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.markedEmoji = {}));
+})(this, (function (exports) { 'use strict';
+
+  const defaultOptions = {
+    // emojis: {}, required
+    unicode: false
+  };
+
+  function markedEmoji(options) {
+    options = {
+      ...defaultOptions,
+      ...options
+    };
+
+    if (!options.emojis) {
+      throw new Error('Must provide emojis to markedEmoji');
+    }
+
+    const emojiNames = Object.keys(options.emojis).map(e => e.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|');
+    const emojiRegex = new RegExp(`:(${emojiNames}):`);
+    const tokenizerRule = new RegExp(`^${emojiRegex.source}`);
+
+    return {
+      extensions: [{
+        name: 'emoji',
+        level: 'inline',
+        start(src) { return src.match(emojiRegex)?.index; },
+        tokenizer(src, tokens) {
+          const match = tokenizerRule.exec(src);
+          if (!match) {
+            return;
+          }
+
+          const name = match[1];
+          const emoji = options.emojis[name];
+
+          return {
+            type: 'emoji',
+            raw: match[0],
+            name,
+            emoji
+          };
+        },
+        renderer(token) {
+          if (options.unicode) {
+            return token.emoji;
+          } else {
+            return `<img alt="${token.name}" src="${token.emoji}"${this.parser.options.xhtml ? ' /' : ''}>`;
+          }
+        }
+      }]
+    };
+  }
+
+  exports.markedEmoji = markedEmoji;
+
+}));
