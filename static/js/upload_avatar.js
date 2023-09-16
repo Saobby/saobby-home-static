8ff6f1cb9892cda624a84a96295d46057bb9d3b0
Saobby <101960076+Saobby@users.noreply.github.com>
2023-09-16 19:04:53 +0800

    修好头像上传，并升级至v2 captcha

--- static/js/upload_avatar.js
+++ static/js/upload_avatar.js
@@ -1,1 +1,104 @@
-var domain="https://aether.saobby.com:5002";function upload_avatar(){void 0!==gebi("avatar_img_file").files[0]?(get_captcha_img(),gebi("captcha-window").hidden=!1):gebi("upload_result").innerHTML="你未选择任何文件"}function set_avatar(){if(""!=(e=gebi("avatar_url").value)){gebi("save-btn").disabled=!0,gebi("save-btn").innerHTML="请稍候";var e=gebi("avatar_url").value,t=new XMLHttpRequest;t.open("POST",domain+"/api/set_avatar_url",!0),t.setRequestHeader("Content-Type","application/json");var n={avatar_url:e,access_token:localStorage.getItem("access-token")};t.send(JSON.stringify(n)),t.onreadystatechange=function(){4==t.readyState&&(ret_json=JSON.parse(t.responseText),ret_json.success?window.location="/":(gebi("save-btn").disabled=!1,gebi("save-btn").innerHTML="保存",gebi("set_result").innerHTML=ret_json.message))}}else gebi("set_result").innerHTML="头像链接不能为空!"}function complete_captcha(){gebi("upload-btn").innerHTML="上传中",gebi("upload-btn").disabled=!0;var e=gebi("avatar_img_file").files[0],t=gebi("captcha-token").value,n=new FormData,a=new XMLHttpRequest;a.open("post","https://saobbyggusercontent.pythonanywhere.com/api/upload_image",!0),a.onreadystatechange=function(){4==this.readyState&&(ret_json=JSON.parse(this.responseText),ret_json.success&&(gebi("avatar_url").value=ret_json.data.image_url,gebi("avatar_img").src=ret_json.data.image_url),gebi("upload_result").innerHTML=ret_json.message,gebi("upload-btn").innerHTML="上传",gebi("upload-btn").disabled=!1)},a.upload.onprogress=function(e){if(e.lengthComputable){var t=100*e.loaded/e.total;gebi("upload_result").innerHTML=100==t?"正在处理...":"上传中("+Math.floor(t)+"%)"}},n.append("image",e),n.append("captcha_token",t),a.send(n)}null==localStorage.getItem("access-token")&&(localStorage.login_redirect=window.location.href,window.location="/login");
+var domain = "https://aether.saobby.com:5002";
+//function upload_avatar() {
+//    void 0 !== gebi("avatar_img_file").files[0] ? (get_captcha_img(),
+//    gebi("captcha-window").hidden = !1) : gebi("upload_result").innerHTML = "你未选择任何文件"
+//}
+function upload_avatar() {
+    var file = gebi("avatar_img_file").files[0];
+    if (file === undefined){
+        gebi("upload_result").innerHTML = "你未选择任何文件";
+        return;
+    }
+    gebi("upload-btn").disabled = true;
+    gebi("avatar_img_file").disabled = true;
+    gebi("upload-btn").innerHTML = "请完成人机验证";
+    saobbyCaptchaV2.open_window_and_return_promise().then(function(val){
+        gebi("upload-btn").innerHTML = "正在上传";
+        var form_data = new FormData;
+        var http = new XMLHttpRequest;
+        http.open("post", "https://aether.saobby.com:5013/api/upload_image", true);
+        http.onreadystatechange = function(){
+            if (this.readyState === 4){
+                var ret = JSON.parse(this.responseText);
+                if (ret.success){
+                    gebi("upload_result").innerHTML = "上传成功";
+                    gebi("avatar_url").value = ret.data.image_url;
+                    gebi("avatar_img").src = ret.data.image_url;
+                }else{
+                    gebi("upload_result").innerHTML = ret.message;
+                }
+                gebi("upload-btn").innerHTML = "上传";
+                gebi("upload-btn").disabled = false;
+                gebi("avatar_img_file").disabled = false;
+            }
+        };
+        http.upload.onprogress = function(t){
+            if (t.lengthComputable){
+                var percentage = 100 * t.loaded / t.total;
+                if (percentage === 100){
+                    gebi("upload_result").innerHTML = "正在处理...";
+                    gebi("upload-progress").hidden = true;
+                }else{
+                    gebi("upload_result").innerHTML = "上传中(" + Math.floor(percentage) + "%)";
+                    gebi("upload-progress").value = percentage;
+                    gebi("upload-progress").hidden = false;
+                }
+            }
+        };
+        form_data.append("image", file);
+        form_data.append("captcha_token", val.captcha_token);
+        http.send(form_data);
+    },
+    function(val){
+        gebi("upload_result").innerHTML = "请先完成人机验证:"+val.message;
+        gebi("upload-btn").disabled = false;
+        gebi("avatar_img_file").disabled = false;
+        gebi("upload-btn").innerHTML = "上传";
+    });
+}
+function set_avatar() {
+    var e, a;
+    "" != (a = gebi("avatar_url").value) ? (gebi("save-btn").disabled = !0,
+    gebi("save-btn").innerHTML = "请稍候",
+    a = gebi("avatar_url").value,
+    (e = new XMLHttpRequest).open("POST", domain + "/api/set_avatar_url", !0),
+    e.setRequestHeader("Content-Type", "application/json"),
+    a = {
+        avatar_url: a,
+        access_token: localStorage.getItem("access-token")
+    },
+    e.send(JSON.stringify(a)),
+    e.onreadystatechange = function() {
+        4 == e.readyState && ((ret_json = JSON.parse(e.responseText)).success ? window.location = "/" : (gebi("save-btn").disabled = !1,
+        gebi("save-btn").innerHTML = "保存",
+        gebi("set_result").innerHTML = ret_json.message))
+    }
+    ) : gebi("set_result").innerHTML = "头像链接不能为空!"
+}
+//function complete_captcha() {
+//    gebi("upload-btn").innerHTML = "上传中",
+//    gebi("upload-btn").disabled = !0;
+//    var e = gebi("avatar_img_file").files[0]
+//      , a = gebi("captcha-token").value
+//      , t = new FormData
+//      , n = new XMLHttpRequest;
+//    n.open("post", "https://saobbyggusercontent.pythonanywhere.com/api/upload_image", !0),
+//    n.onreadystatechange = function() {
+//        4 == this.readyState && ((ret_json = JSON.parse(this.responseText)).success && (gebi("avatar_url").value = ret_json.data.image_url,
+//        gebi("avatar_img").src = ret_json.data.image_url),
+//        gebi("upload_result").innerHTML = ret_json.message,
+//        gebi("upload-btn").innerHTML = "上传",
+//        gebi("upload-btn").disabled = !1)
+//    }
+//    ,
+//    n.upload.onprogress = function(e) {
+//        e.lengthComputable && (e = 100 * e.loaded / e.total,
+//        gebi("upload_result").innerHTML = 100 == e ? "正在处理..." : "上传中(" + Math.floor(e) + "%)")
+//    }
+//    ,
+//    t.append("image", e),
+//    t.append("captcha_token", a),
+//    n.send(t)
+//}
+null == localStorage.getItem("access-token") && (localStorage.login_redirect = window.location.href,
+window.location = "/login");
