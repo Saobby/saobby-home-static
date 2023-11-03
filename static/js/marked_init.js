27d8bf233d56034016dc4aa5dee054f12511a839
Saobby <101960076+Saobby@users.noreply.github.com>
2023-11-03 19:36:20 +0800

    Update marked_init.js

--- static/js/marked_init.js
+++ static/js/marked_init.js
@@ -21,7 +21,7 @@
     mdr.codespan = function(e) {
         var t;
         return e.match(/^网易云音乐:\d+$/) ? (t = Math.random(),
-        `<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src_="//music.163.com/outchain/player?type=2&id=${e.match(/\d+$/)[0]}&auto=0&height=66" id="music-player-${t}" hidden></iframe><a onclick="!function(t){var e=gebi('music-player-${t}');e.src=e.getAttribute('src_');e.hidden=!1;t.hidden=!0}(this);" href="javascript:;">点击加载网易云音乐</a>`) : "<code>" + e + "</code>"
+        `<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src_="https://music.163.com/outchain/player?type=2&id=${e.match(/\d+$/)[0]}&auto=0&height=66" id="music-player-${t}" hidden></iframe><a onclick="!function(t){var e=gebi('music-player-${t}');e.src=e.getAttribute('src_');e.hidden=!1;t.hidden=!0}(this);" href="javascript:;">点击加载网易云音乐</a>`) : "<code>" + e + "</code>"
     }
     marked.setOptions({
         tables: true,
