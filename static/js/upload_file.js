87bc50960a6bc252c0e627a7ceb5042efb287f38
Saobby <101960076+Saobby@users.noreply.github.com>
2025-02-22 08:51:33 +0800

    升级到captchav3() (13/?)

--- static/js/upload_file.js
+++ static/js/upload_file.js
@@ -1,52 +1,58 @@
-function upload_file() {
+async function upload_file() {
     var file = gebi("file").files[0];
     if (file === undefined){
         gebi("upload_result").innerHTML = "你未选择任何文件";
         return;
     }
-    set_btn_html(gebi("upload-btn"), "请完成人机验证");
+    set_btn_html(gebi("upload-btn"), "...");
     gebi("file").disabled = true;
-    saobbyCaptchaV2.open_window_and_return_promise().then(function(val){
-        set_btn_html(gebi("upload-btn"), "正在上传");
-        var form_data = new FormData;
-        var http = new XMLHttpRequest;
-        http.open("post", "https://github-picbed.saobby.com/api/github_picbed", true);
-        http.onreadystatechange = function(){
-            if (this.readyState === 4){
-                var ret = JSON.parse(this.responseText);
-                if (ret.success){
-                    gebi("upload_result").innerHTML = "上传成功";
-                    gebi("file_url").value = ret.data.url;
-                    gebi("markdown").value = "![](" + ret.data.url + ")";
-                }else{
-                    gebi("upload_result").innerHTML = ret.message;
-                }
-                set_btn_html(gebi("upload-btn"));
-                gebi("file").disabled = false;
+    const captcha_rsp = await captcha_v3();
+    if (captcha_rsp.retcode){
+        gebi("upload_result").innerHTML = "人机验证失败:"+captcha_rsp.msg;
+        set_btn_html(gebi("upload-btn"));
+        gebi("file").disabled = false;
+        return;
+    }
+
+    set_btn_html(gebi("upload-btn"), "正在上传");
+    var form_data = new FormData;
+    var http = new XMLHttpRequest;
+    http.open("post", "https://github-picbed.saobby.com/api/github_picbed", true);
+    http.onreadystatechange = function(){
+        if (this.readyState === 4){
+            var ret = JSON.parse(this.responseText);
+            if (ret.success){
+                gebi("upload_result").innerHTML = "上传成功";
+                gebi("file_url").value = ret.data.url;
+                gebi("markdown").value = "![](" + ret.data.url + ")";
+            }else{
+                gebi("upload_result").innerHTML = ret.message;
             }
-        };
-        http.upload.onprogress = function(t){
-            if (t.lengthComputable){
-                var percentage = 100 * t.loaded / t.total;
-                if (percentage === 100){
-                    gebi("upload_result").innerHTML = "正在处理...";
-                    gebi("upload-progress").hidden = true;
-                }else{
-                    gebi("upload_result").innerHTML = "上传中(" + Math.floor(percentage) + "%)";
-                    gebi("upload-progress").value = percentage;
-                    gebi("upload-progress").hidden = false;
-                }
+            set_btn_html(gebi("upload-btn"));
+            gebi("file").disabled = false;
+        }
+    };
+    http.upload.onprogress = function(t){
+        if (t.lengthComputable){
+            var percentage = 100 * t.loaded / t.total;
+            if (percentage === 100){
+                gebi("upload_result").innerHTML = "正在处理...";
+                gebi("upload-progress").hidden = true;
+            }else{
+                gebi("upload_result").innerHTML = "上传中(" + Math.floor(percentage) + "%)";
+                gebi("upload-progress").value = percentage;
+                gebi("upload-progress").hidden = false;
             }
-        };
-        form_data.append("file", file);
-        form_data.append("captcha_token", val.captcha_token);
-        http.send(form_data);
-    },
-    function(val){
-        gebi("upload_result").innerHTML = "请先完成人机验证:"+val.message;
+        }
+    };
+    http.onerror = function(){
+        gebi("upload_result").innerHTML = "网络错误";
         gebi("file").disabled = false;
         set_btn_html(gebi("upload-btn"));
-    });
+    };
+    form_data.append("file", file);
+    form_data.append("captcha_token", captcha_rsp.data.token);
+    http.send(form_data);
 }
 //function complete_captcha() {
 //    gebi("upload-btn").innerHTML = "上传中",
