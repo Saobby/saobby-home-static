333c77a8d8d045e454b0b180a5b3f4b0e85cb2bc
Saobby <101960076+Saobby@users.noreply.github.com>
2025-02-22 08:47:38 +0800

    升级到captchav3() (12/?)

--- static/js/upload_avatar.js
+++ static/js/upload_avatar.js
@@ -3,55 +3,62 @@ var domain = "https://comments.saobby.com";
 //    void 0 !== gebi("avatar_img_file").files[0] ? (get_captcha_img(),
 //    gebi("captcha-window").hidden = !1) : gebi("upload_result").innerHTML = "你未选择任何文件"
 //}
-function upload_avatar() {
+async function upload_avatar() {
     var file = gebi("avatar_img_file").files[0];
     if (file === undefined){
         gebi("upload_result").innerHTML = "你未选择任何文件";
         return;
     }
-    set_btn_html(gebi("upload-btn"), "请完成人机验证");
+    set_btn_html(gebi("upload-btn"), "...");
     gebi("avatar_img_file").disabled = true;
-    saobbyCaptchaV2.open_window_and_return_promise().then(function(val){
-        set_btn_html(gebi("upload-btn"), "正在上传");
-        var form_data = new FormData;
-        var http = new XMLHttpRequest;
-        http.open("post", "https://image.saobby.com/api/upload_image", true);
-        http.onreadystatechange = function(){
-            if (this.readyState === 4){
-                var ret = JSON.parse(this.responseText);
-                if (ret.success){
-                    gebi("upload_result").innerHTML = "上传成功";
-                    gebi("avatar_url").value = ret.data.image_url;
-                    gebi("avatar_img").src = ret.data.image_url;
-                }else{
-                    gebi("upload_result").innerHTML = ret.message;
-                }
-                set_btn_html(gebi("upload-btn"));
-                gebi("avatar_img_file").disabled = false;
+
+    const captcha_rsp = await captcha_v3();
+    if (captcha_rsp.retcode){
+        gebi("upload_result").innerHTML = "人机验证失败:"+captcha_rsp.msg;
+        set_btn_html(gebi("upload-btn"));
+        gebi("avatar_img_file").disabled = false;
+        return;
+    }
+
+    set_btn_html(gebi("upload-btn"), "正在上传");
+    var form_data = new FormData;
+    var http = new XMLHttpRequest;
+    http.open("post", "https://image.saobby.com/api/upload_image", true);
+    http.onreadystatechange = function(){
+        if (this.readyState === 4){
+            var ret = JSON.parse(this.responseText);
+            if (ret.success){
+                gebi("upload_result").innerHTML = "上传成功";
+                gebi("avatar_url").value = ret.data.image_url;
+                gebi("avatar_img").src = ret.data.image_url;
+            }else{
+                gebi("upload_result").innerHTML = ret.message;
             }
-        };
-        http.upload.onprogress = function(t){
-            if (t.lengthComputable){
-                var percentage = 100 * t.loaded / t.total;
-                if (percentage === 100){
-                    gebi("upload_result").innerHTML = "正在处理...";
-                    gebi("upload-progress").hidden = true;
-                }else{
-                    gebi("upload_result").innerHTML = "上传中(" + Math.floor(percentage) + "%)";
-                    gebi("upload-progress").value = percentage;
-                    gebi("upload-progress").hidden = false;
-                }
+            set_btn_html(gebi("upload-btn"));
+            gebi("avatar_img_file").disabled = false;
+        }
+    };
+    http.upload.onprogress = function(t){
+        if (t.lengthComputable){
+            var percentage = 100 * t.loaded / t.total;
+            if (percentage === 100){
+                gebi("upload_result").innerHTML = "正在处理...";
+                gebi("upload-progress").hidden = true;
+            }else{
+                gebi("upload_result").innerHTML = "上传中(" + Math.floor(percentage) + "%)";
+                gebi("upload-progress").value = percentage;
+                gebi("upload-progress").hidden = false;
             }
-        };
-        form_data.append("image", file);
-        form_data.append("captcha_token", val.captcha_token);
-        http.send(form_data);
-    },
-    function(val){
-        gebi("upload_result").innerHTML = "请先完成人机验证:"+val.message;
+        }
+    };
+    http.onerror = function(){
+        gebi("upload_result").innerHTML = "网络错误";
         gebi("avatar_img_file").disabled = false;
         set_btn_html(gebi("upload-btn"));
-    });
+    };
+    form_data.append("image", file);
+    form_data.append("captcha_token", captcha_rsp.data.token);
+    http.send(form_data);
 }
 async function set_avatar() {
     let avatar_url = gebi("avatar_url").value;
