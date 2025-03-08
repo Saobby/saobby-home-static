1ef2017f68f0285e01192a423ff0d0b73996d422
Saobby <101960076+Saobby@users.noreply.github.com>
2025-03-08 21:26:43 +0800

    我的计数器添加搜索功能

--- static/js/my_webcounters.js
+++ static/js/my_webcounters.js
@@ -12,7 +12,8 @@ async function load_webcounters(page_index) {
     const payload = {
         access_token: localStorage.getItem("access-token"), 
         page_index: page_index, 
-        page_size: 8
+        page_size: 8, 
+        keyword: gebi("search-input").value
     };
     const rsp = await fetch_api(domain+"/api/get_users_counter", payload);
     set_btn_status(false);
@@ -23,7 +24,7 @@ async function load_webcounters(page_index) {
     let html = "";
     for (let i = 0; i < rsp.data.counters.length; i++){
         const counter = rsp.data.counters[i];
-        html += `<div class="bottom-line"><b style="font-size:20px;">${counter.desc?counter.desc:counter.counter_id}</b><br><span style="color:#777;">${icon_with_text("clock-grey", ts2str(counter.create_at))}</span><br><a href="/webcounter_dashboard?access_token=${counter.access_token}" target="_blank"><button class="wux-btn wux-btn-primary wux-btn-sm">${icon_with_text("external-link-white", "进入控制面板")}</button></a></div>`;
+        html += `<div class="bottom-line"><b style="font-size:20px;">${counter.desc?counter.desc:counter.counter_id}</b><br><span style="color:#777;">${icon_with_text("clock-grey", ts2str(counter.create_at))}</span> <span style="color:#777;">${icon_with_text("typeface-grey", "")}${["JPEG", "SVG"][counter.pic_type]}·${["访问量", "IP数", "不显示"][counter.display_type]}</span><br><a href="/webcounter_dashboard?access_token=${counter.access_token}" target="_blank"><button class="wux-btn wux-btn-primary wux-btn-sm">${icon_with_text("external-link-white", "进入控制面板")}</button></a></div>`;
     }
     gebi("webcounters").innerHTML = html;
     gen_cp_buttons(rsp.data.page_index+1, rsp.data.page_amount, 8, (n)=>{load_webcounters(n-1).then();}, gebi("change-page-div"), "wux-btn cp-btn", "wux-btn wux-btn-outline cp-btn");
