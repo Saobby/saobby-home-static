a99f7939211961e562dd72f461a4598c7f4c14cf
Saobby <101960076+Saobby@users.noreply.github.com>
2025-03-08 10:58:59 +0800

    添加设置功能

--- static/js/webcounter_dashboard.js
+++ static/js/webcounter_dashboard.js
@@ -1,5 +1,6 @@
 domain = "http://127.0.0.1:7799";
 access_token = get_url_args().access_token;
+tab = "overview"
 
 
 async function load_logs(page_index) {
@@ -9,6 +10,12 @@ async function load_logs(page_index) {
         const cp_btns = gebcn("log-cp-btn");
         for (let i = 0; i < cp_btns.length; i++){
             cp_btns[i].disabled = status;
+        }
+        if (status){
+            set_btn_html(gebi("refresh-log-btn"), "...");
+        }
+        else{
+            set_btn_html(gebi("refresh-log-btn"));
         }
     }
     const payload = {
@@ -50,7 +57,7 @@ async function load_logs(page_index) {
 function update_auto_refresh_log(checked){
     if (checked){
         auto_refresh_interval = setInterval(() => {
-            if (document.visibilityState === "visible"){
+            if (document.visibilityState === "visible" && tab === "log"){
                 load_logs(0).then();
             }
         }, 5000);
@@ -59,4 +66,62 @@ function update_auto_refresh_log(checked){
         clearInterval(auto_refresh_interval);
         auto_refresh_interval = null;
     }
+}
+
+async function load_settings() {
+    function set_btn_status(status){
+        gebi("save-settings-btn").disabled = status;
+        gebi("image-type-select").disabled = status;
+        gebi("display-setting-select").disabled = status;
+        gebi("font-size").disabled = status;
+        gebi("font-color").disabled = status;
+        gebi("background-color").disabled = status;
+    }
+    set_btn_status(true);
+    const rsp = await fetch_api(domain+"/api/get_counter_settings", {access_token: access_token});
+    set_btn_status(false);
+    if (rsp.retcode){
+        gebi("save-settings-result").innerHTML = "无法加载设置:"+rsp.msg;
+        return;
+    }
+    gebi("counter-id").innerHTML = rsp.data.settings.counter_id;
+    gebi("access-token").innerHTML = rsp.data.settings.access_token;
+    gebi("image-type-select").selectedIndex = rsp.data.settings.pic_type;
+    gebi("display-setting-select").selectedIndex = rsp.data.settings.display_type;
+    gebi("font-size").value = rsp.data.settings.font_size;
+    gebi("font-color").value = rsp.data.settings.text_color;
+    gebi("background-color").value = rsp.data.settings.background_color;
+}
+
+async function save_settings(){
+    function set_btn_status(status){
+        gebi("save-settings-btn").disabled = status;
+        gebi("image-type-select").disabled = status;
+        gebi("display-setting-select").disabled = status;
+        gebi("font-size").disabled = status;
+        gebi("font-color").disabled = status;
+        gebi("background-color").disabled = status;
+        if (status){
+            set_btn_html(gebi("save-settings-btn"), "...");
+        }
+        else{
+            set_btn_html(gebi("save-settings-btn"));
+        }
+    }
+    const payload = {
+        access_token: access_token,
+        pic_type: gebi("image-type-select").selectedIndex,
+        display_type: gebi("display-setting-select").selectedIndex,
+        font_size: gebi("font-size").value,
+        text_color: gebi("font-color").value,
+        background_color: gebi("background-color").value
+    }
+    set_btn_status(true);
+    const rsp = await fetch_api(domain+"/api/update_counter_settings", payload);
+    set_btn_status(false);
+    if (rsp.retcode){
+        gebi("save-settings-result").innerHTML = "无法保存设置:"+rsp.msg;
+        return;
+    }
+    gebi("save-settings-result").innerHTML = "设置已保存";
 }
