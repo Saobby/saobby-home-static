2edf24c53808f3613379fad122a5e242552481d9
Saobby <101960076+Saobby@users.noreply.github.com>
2025-03-08 22:10:27 +0800

    控制面板添加登录功能

--- static/js/webcounter_dashboard.js
+++ static/js/webcounter_dashboard.js
@@ -2,6 +2,33 @@ domain = "http://127.0.0.1:7799";
 access_token = get_url_args().access_token;
 tab = "overview"
 
+async function login() {
+    function set_btn_status(status){
+        gebi("login-btn").disabled = status;
+        if (status){
+            set_btn_html(gebi("login-btn"), "...");
+        }
+        else{
+            set_btn_html(gebi("login-btn"));
+        }
+    }
+    const access_token = gebi("access-token-input").value;
+    if (!access_token){
+        gebi("login-result").innerHTML = "请输入访问密钥";
+        return;
+    }
+    const payload = {
+        access_token: access_token
+    }
+    set_btn_status(true);
+    const rsp = await fetch_api(domain + "/api/verify_access_token", payload);
+    set_btn_status(false);
+    if (rsp.retcode){
+        gebi("login-result").innerHTML = rsp.msg;
+        return;
+    }
+    window.location.href = "/webcounter_dashboard?access_token=" + access_token;
+}
 
 async function load_logs(page_index) {
     function set_btn_status(status){
@@ -712,6 +739,10 @@ async function render_ip_location_chart() {
         gebi("ip-location-result").innerHTML = "加载失败:" + rsp.msg;
         return;
     }
+    if (JSON.stringify(rsp.data.visits) === "{}"){
+        gebi("ip-location-result").innerHTML = "暂无数据";
+        return;
+    }
 
     const country_code_map = {'AF': '阿富汗', 'AL': '阿尔巴尼亚', 'DZ': '阿尔及利亚', 'AO': '安哥拉', 'AR': '阿根廷', 'AM': '亚美尼亚', 'AU': '澳大利亚', 'AT': '奥地利', 'AZ': '阿塞拜疆', 'BH': '巴林', 'BD': '孟加拉国', 'BY': '白俄罗斯', 'BE': '比利时', 'BZ': '伯利兹', 'BJ': '贝宁', 'BT': '不丹', 'BO': '玻利维亚', 'BA': '波斯尼亚', 'BW': '博茨瓦纳', 'BR': '巴西', 'BN': '文莱', 'BG': '保加利亚', 'BF': '布基纳法索', 'BI': '布隆迪', 'KH': '柬埔寨', 'CM': '喀麦隆', 'CA': '加拿大', 'CF': '中非', 'TD': '乍得', 'CL': '智利', 'CN': '中国', 'CO': '哥伦比亚', 'KM': '科摩罗', 'CG': '刚果', 'CD': '刚果', 'CR': '哥斯达黎加', 'CI': '科特迪瓦', 'HR': '克罗地亚', 'CU': '古巴', 'CY': '塞浦路斯', 'CZ': '捷克', 'DK': '丹麦', 'DJ': '吉布提', 'DO': '多米尼加', 'EC': '厄瓜多尔', 'EG': '埃及', 'SV': '萨尔瓦多', 'GQ': '几内亚', 'ER': '厄立特里亚', 'EE': '爱沙尼亚', 'SZ': '斯威士兰', 'ET': '埃塞俄比亚', 'FJ': '斐济', 'FI': '芬兰', 'FR': '法国', 'GA': '加蓬', 'GM': '冈比亚', 'GE': '格鲁吉亚', 'DE': '德国', 'GH': '加纳', 'GR': '希腊', 'GT': '危地马拉', 'GN': '几内亚', 'GW': '几内亚比绍', 'GY': '圭亚那', 'HT': '海地', 'HN': '洪都拉斯', 'HU': '匈牙利', 'IS': '冰岛', 'IN': '印度', 'ID': '印度尼西亚', 'IR': '伊朗', 'IQ': '伊拉克', 'IE': '爱尔兰', 'IL': '以色列', 'IT': '意大利', 'JM': '牙买加', 'JP': '日本', 'JO': '约旦', 'KZ': '哈萨克斯坦', 'KE': '肯尼亚', 'KR': '韩国', 'KW': '科威特', 'KG': '吉尔吉斯斯坦', 'LA': '老挝', 'LV': '拉脱维亚', 'LB': '黎巴嫩', 'LS': '莱索托', 'LR': '利比里亚', 'LY': '利比亚', 'LT': '立陶宛', 'LU': '卢森堡', 'MG': '马达加斯加', 'MW': '马拉维', 'MY': '马来西亚', 'MV': '斯里兰卡', 'ML': '马里', 'MT': '马耳他', 'MR': '毛里塔尼亚', 'MU': '毛里求斯', 'MX': '墨西哥', 'MD': '摩尔多瓦', 'MC': '法国', 'MN': '蒙古', 'ME': '黑山', 'MA': '摩洛哥', 'MZ': '莫桑比克', 'MM': '缅甸', 'NA': '纳米比亚', 'NP': '尼泊尔', 'NL': '荷兰', 'NZ': '新西兰', 'NI': '尼加拉瓜', 'NE': '尼日尔', 'NG': '尼日利亚', 'MK': '马其顿', 'NO': '挪威', 'OM': '阿曼', 'PK': '巴基斯坦', 'PA': '巴拿马', 'PG': '巴布亚新几内亚', 'PY': '巴拉圭', 'PE': '秘鲁', 'PH': '菲律宾', 'PL': '波兰', 'PT': '葡萄牙', 'QA': '卡塔尔', 'RO': '罗马尼亚', 'RU': '俄罗斯', 'RW': '卢旺达', 'SA': '沙特阿拉伯', 'SN': '塞内加尔', 'RS': '塞尔维亚', 'SL': '塞拉利昂', 'SG': '新加坡', 'SK': '斯洛伐克', 'SI': '斯洛文尼亚', 'SO': '索马里', 'ZA': '南非', 'ES': '西班牙', 'LK': '斯里兰卡', 'SD': '苏丹', 'SR': '苏里南', 'SE': '瑞典', 'CH': '瑞士', 'SY': '叙利亚', 'TJ': '塔吉克斯坦', 'TZ': '坦桑尼亚', 'TH': '泰国', 'TG': '多哥', 'TO': '汤加', 'TT': '特立尼达和多巴哥', 'TN': '突尼斯', 'TR': '土耳其', 'TM': '土库曼斯坦', 'TW': '中国', 'UG': '乌干达', 'UA': '乌克兰', 'AE': '阿联酋', 'GB': '英国', 'US': '美国', 'UY': '乌拉圭', 'UZ': '乌兹别克斯坦', 'VU': '瓦努阿图', 'VE': '委内瑞拉', 'VN': '越南', 'YE': '也门', 'ZM': '赞比亚', 'ZW': '津巴布韦'};
 
@@ -815,13 +846,20 @@ async function render_ip_location_chart() {
 }
 
 async function init(){
-    const [echarts_js, world_json] = await Promise.all([
-        fetch("/static/js/echarts.min.js").then(response => response.text()),
-        fetch("/static/js/world_cn.json").then(response => response.json())
-    ]);
-    eval(echarts_js);
-    echarts.registerMap("world", world_json);
-    load_overview_page().then();
+    if (access_token){
+        gebi("login").hidden = true;
+        gebi("dashboard").hidden = false;
+        const [echarts_js, world_json] = await Promise.all([
+            fetch("/static/js/echarts.min.js").then(response => response.text()),
+            fetch("/static/js/world_cn.json").then(response => response.json())
+        ]);
+        eval(echarts_js);
+        echarts.registerMap("world", world_json);
+        load_overview_page().then();
+    }else{
+        gebi("login").hidden = false;
+        gebi("dashboard").hidden = true;
+    }
 }
 
 init().then();
