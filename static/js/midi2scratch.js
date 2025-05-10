088cf3d8c21381f956a96eedcdf500fb2537e065
Saobby <101960076+saobby@users.noreply.github.com>
2025-05-10 18:33:12 +0800

    midi2scratch 工具:更换到大陆访问更稳定的 soundfont 域名

--- static/js/midi2scratch.js
+++ static/js/midi2scratch.js
@@ -295,7 +295,7 @@ async function upload_midi() {
 async function load_midi_player(){
     await load_script("/static/js/midi_player.js");
     gebi("midi-player-div").innerHTML = `
-    <midi-player src="" sound-font="https://soundfont.saobby.com/midi" id="midi-player"></midi-player>
+    <midi-player src="" sound-font="https://soundfont-fast.saobby.com/midi" id="midi-player"></midi-player>
     <midi-visualizer type="piano-roll" id="midi-visualizer"></midi-visualizer>
     `;
     gebi("midi-visualizer").config = {
