b8b8bcea747c1afe8e496314fa09d95aaf398029
Saobby <101960076+saobby@users.noreply.github.com>
2025-12-19 22:48:13 +0800

    检查生产环境包版本

--- v2/vite.config.js
+++ v2/vite.config.js
@@ -55,9 +55,9 @@ export default defineConfig({
         minify: 'terser',
         terserOptions: {
             compress: {
-                drop_console: true,
+                drop_console: false,
                 drop_debugger: true,
-                pure_funcs: ['console.log']
+                pure_funcs: []
             },
             mangle: {
                 toplevel: true,
